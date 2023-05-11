bc591684b9b1cb0d0b1b6f1e800aba0573232600
anthony <anbacus@yahoo.com>
2023-05-11 16:16:00 +0100

    files added

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>My Simple Website</title>
+</head>
+<body>
+	<header>
+		<h1>Welcome to My Simple Website!</h1>
+		<nav>
+			<ul>
+				<li><a href="index.php">Home</a></li>
+				<li><a href="about.php">About</a></li>
+			</ul>
+		</nav>
+	</header>
+	<main>
+		<p>Hello, World! This is my simple website.</p>
+	</main>
+	<footer>
+		<p>&copy; 2023 My Simple Website</p>
+	</footer>
+</body>
+</html>
